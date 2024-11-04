381d3bcd1a4cc613c1b2d88fdbd80553e41b61d1
Emanuel Grigolatto <ema_grigo@hotmail.com>
2024-11-03 21:51:09 -0300

    completed exercices 7.4-7.6

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -7,6 +7,7 @@ import {
   useNavigate,
   useMatch,
 } from "react-router-dom";
+import AnecdoteForm from "./components/AnecdoteForm";
 
 const Menu = () => {
   const padding = {
@@ -88,60 +89,7 @@ const Footer = () => (
   </footer>
 );
 
-const AnecdoteForm = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
 
-  const navigate = useNavigate();
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    props.addNew({
-      content,
-      author,
-      info,
-      votes: 0,
-    });
-    setContent("");
-    setAuthor("");
-    setInfo("");
-    navigate("/");
-  };
-
-  return (
-    <div>
-      <h2>create a new anecdote</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
-        </div>
-        <div>
-          author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
-        </div>
-        <div>
-          url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
-        </div>
-        <button>create</button>
-      </form>
-    </div>
-  );
-};
 
 const Notification = ({ message }) => {
   return <p>{message}</p>;
---
+++ part7/routed-anecdotes/src/components/AnecdoteForm.jsx
@@ -0,0 +1,56 @@
+import { useNavigate } from "react-router-dom";
+import { useField } from "../hooks";
+
+const AnecdoteForm = (props) => {
+  const { reset: resetContent, ...content } = useField("text");
+  const { reset: resetAuthor, ...author } = useField("text");
+  const { reset: resetInfo, ...info } = useField("text");
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    props.addNew({
+      content: content.value,
+      author: author.value,
+      info: info.value,
+      votes: 0,
+    });
+    resetContent();
+    resetAuthor();
+    resetInfo();
+    navigate("/");
+  };
+
+  const handleReset = () => {
+    resetContent();
+    resetAuthor();
+    resetInfo();
+  };
+
+  return (
+    <div>
+      <h2>create a new anecdote</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          content
+          <input {...content} />
+        </div>
+        <div>
+          author
+          <input {...author} />
+        </div>
+        <div>
+          url for more info
+          <input {...info} />
+        </div>
+        <button type="submit">create</button>
+        <button type="button" onClick={handleReset}>
+          reset
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AnecdoteForm;
